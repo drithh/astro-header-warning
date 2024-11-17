5eccd839ae680c33cecd052c3525261a5a94db83
Adriel Alfeus <adrielalfeus@gmail.com>
2024-11-18 00:13:14 +0700

    add repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  security: {
+    checkOrigin: true,
+  },
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
